430d93571a3fcb22891d8cdd9c4e27a995ff53ef
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-07-12 14:16:44 -0400

    fetching data

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,12 +1,15 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-};
+var getUserRepos = function (user) {
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-
-getUserRepos();
-
-var response = {
-    location: "San Francisco",
+    //make a request to the url
+    
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+        })
+    })
+    
 }
 
-console.log(response.location);
+getUserRepos("facebook");
