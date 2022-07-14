5f58e5e8224d3c2edfd0df6fda2a1ad23073428d
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-07-14 13:35:46 -0400

    list issues added

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -26,25 +26,25 @@ var getUserRepos = function (user) {
     //make a request to the url
 
     fetch(apiUrl)
-    .then(function(response) {
-      // request was successful
-      if (response.ok) {
-        response.json().then(function(data) {
-          displayRepos(data, user);
+        .then(function (response) {
+            // request was successful
+            if (response.ok) {
+                response.json().then(function (data) {
+                    displayRepos(data, user);
+                });
+            } else {
+                alert('Error: GitHub User Not Found');
+            }
+        })
+        .catch(function (error) {
+            // Notice this `.catch()` getting chained onto the end of the `.then()` method
+            alert("Unable to connect to GitHub");
         });
-      } else {
-        alert('Error: GitHub User Not Found');
-      }
-    })
-    .catch(function(error) {
-      // Notice this `.catch()` getting chained onto the end of the `.then()` method
-      alert("Unable to connect to GitHub");
-    });
 }
 
 var displayRepos = function (repos, searchTerm) {
     // check if api returned any repos
-    if(repos.length === 0){
+    if (repos.length === 0) {
         repoContainerEl.textContent = "No repositories found."
         return;
     }
